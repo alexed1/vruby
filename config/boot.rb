162bc5744ec159eba620292bde1093d1eefe49c5
Alex Edelstein <alex@edelstein.org>
2012-12-30 17:06:00 -0800

    added a line to make dm work with heroku

--- config/boot.rb
+++ config/boot.rb
@@ -5,3 +5,6 @@ require 'debugger'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+require 'dm-rails'
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'mysql://localhost/vruby_development.db')
